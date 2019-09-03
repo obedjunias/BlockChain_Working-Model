eb2d1df78f9fa1d991279b0b2dad35dbd4b29d98
Obed Junias <obedjunias.cs17@bmsce.ac.in>
2019-09-03 21:16:05 +0530

    processing command line arguments

--- Block.js
+++ Block.js
@@ -12,11 +12,17 @@ class Block{
         return hash.SHA256(this.timestamp+JSON.stringify(this.transactions)+this.previoushash+this.nonce).toString();
     }
     mineBlock(difficulty){
+        for(var each of this.transactions){
+            if(each.fromAddress === null){
+                console.log("Now Mining Miner's Block...")
+            }
+        }
+        console.log("Block with ",this.transactions[0].fromAddress," as its first transaction started...");
         while(this.hash.substring(0,difficulty) !== Array(difficulty+1).join("0")){
             this.nonce++;
             this.hash = this.calculateHash();
         }
-        console.log("Block Mined");
+        console.log("Block with ",this.transactions[this.transactions.length-1].fromAddress," as its last transaction mined successfully...");
     }
 }
 
--- main.js
+++ main.js
@@ -3,16 +3,26 @@ const Block = require('./Block');
 const Transaction = require('./Transaction')
 
 let mynewCoin = new BlockChain();
-mynewCoin.createTransaction(new Transaction("Obed","Junias",100))
-mynewCoin.createTransaction(new Transaction("Obed","Junias",50))
+const trs = process.argv.slice(2);
+const l = trs.length/3;
+var ct = 0;
+for (var i=0;i<l;i++){
+    var from = trs[ct++];
+    var to = trs[ct++];
+    var amount = trs[ct++];
+    mynewCoin.createTransaction(new Transaction(from,to,Number(amount)));
+}
+// mynewCoin.createTransaction(new Transaction("Obed","Junias",100))
+// mynewCoin.createTransaction(new Transaction("Obed","Junias",50))
+// console.log(mynewCoin.pendingTransactions)
+mynewCoin.minePendingTransactions("Myaddress")
+console.log("Miner's Bal: ",mynewCoin.getBalanceOf("Myaddress"))
 
 mynewCoin.minePendingTransactions("Myaddress")
-console.log("My Bal: ",mynewCoin.getBalanceOf("Myaddress"))
-
-mynewCoin.minePendingTransactions("Myaddress")
-console.log("My Bal: ",mynewCoin.getBalanceOf("Myaddress"))
+console.log("Miner's Bal: ",mynewCoin.getBalanceOf("Myaddress"))
 // console.log(mynewCoin);
 mynewCoin.minePendingTransactions("Myaddress")
-console.log("My Bal: ",mynewCoin.getBalanceOf("Myaddress"))
+console.log("Miner's Bal: ",mynewCoin.getBalanceOf("Myaddress"))
 // console.log(mynewCoin);
-console.log(mynewCoin.pendingTransactions)
+// console.log(mynewCoin.pendingTransactions)
+// console.log(mynewCoin)
